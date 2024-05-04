33af042df5b03f11ef8ee2e4f3a3753ab8360aa9
pritam khaplee <pritamkhaple8585@gmail.com>
2024-05-04 13:24:12 +0530

    till 17-50hrs commit

---
+++ 2-actual-backend/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { getStoredItems, storeItems } = require('./data/items');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.get('/items', async (req, res) => {
+  const storedItems = await getStoredItems();
+  // await new Promise((resolve, reject) => setTimeout(() => resolve(), 4000));
+  res.json({ items: storedItems });
+});
+
+app.get('/items/:id', async (req, res) => {
+  const storedItems = await getStoredItems();
+  const item = storedItems.find((item) => item.id === req.params.id);
+  res.json({ item });
+});
+
+app.post('/items', async (req, res) => {
+  const existingItems = await getStoredItems();
+  const itemData = req.body;
+  const newItem = {
+    ...itemData,
+    id: Math.random().toString(),
+  };
+  const updatedItems = [newItem, ...existingItems];
+  await storeItems(updatedItems);
+  res.status(201).json({ message: 'Stored new item.', item: newItem });
+});
+
+app.listen(8080);
+
+const PORT = 3000; // Use a different port number
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
